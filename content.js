e7f6080db522dbad883a26733ecafdb57941416a
Daniel Masake Kitui <daniel.kitui@andela.com>
2019-09-04 20:38:24 +0300

    Refactor to enable running on pageload

--- content.js
+++ content.js
@@ -1,11 +1,22 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
-      alert(firstHref);
-    }
-    else if (request.message === 'report_back') {
-	    sendResponse(document.getElementsByClassName("download"));
-    }
-  }
-);
+const addDownloadBtn = () => {
+  let magneticLink;
+  const link =  $(".download")[0];
+  magneticLink = $(link).find('a')[0].href;
+  
+  const loadingAnimation = document.createElement("IMG"); //
+  loadingAnimation.alt = "Loading...be patient";
+  loadingAnimation.setAttribute('class', 'loading');
+  loadingAnimation.src = chrome.runtime.getURL("static/bar.gif");
+  
+  $('#detailsframe').before('<div class="container-div"></div>').slideDown(10000);
+  $('.container-div').append('<button class="addButton" >Add To Download List</button>').hide().fadeIn(1000);
+  $('.addButton').on('click', () => {
+    $('.addButton').replaceWith(loadingAnimation);
+    apiScript(magneticLink)
+  });
+  return true
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(addDownloadBtn, 1500)
+});
