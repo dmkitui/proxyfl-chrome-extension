d5eece8e2a4d32a7da8bc6fc30ca0ca42d0b4d84
Daniel Masake Kitui <daniel.kitui@andela.com>
2019-09-04 15:19:17 +0300

    Refactor to make api call using credentials from local storage

--- api.js
+++ api.js
@@ -1,26 +1,33 @@
-function apiScript (data) {
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", 'https://torrents-api.herokuapp.com/torrent/', true);
-	
-	//Send the proper header information along with the request
-	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	xhr.setRequestHeader("token", "emdl%4E60PLWzVpuZomzxQej1U0pMIBYZ1$#n2DEg@j8uP^Ikp7h#0m1qWLU#K0S");
-	xhr.setRequestHeader("password", "F^J)TUbHipAdQ2*iWznrovj1A3Gy$YYg");
-	xhr.setRequestHeader("magnet", data);
-	
-	xhr.send()
-	
-	xhr.onload = function() {
-	  if (xhr.status !== 201) { // analyze HTTP status of the response
-	  	if ( xhr.status === 409) {
-	  		alert('Error! That file has already been lined up for downloading.')
-		  } else {
-	  		alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+const apiScript = (magnet) => {
+	const apiCall = async (data) => {
+		
+		const {token, url, password} = data;
+		
+		const xhr = new XMLHttpRequest();
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		xhr.setRequestHeader("token", token);
+		xhr.setRequestHeader("password", password);
+		xhr.setRequestHeader("magnet", magnet);
+		xhr.send();
+		
+		xhr.onload = function() {
+		  if (xhr.status !== 201) {
+		    $('.container-div').addClass('error');
+		    if ( xhr.status === 409) {
+		      $('.container-div').text('That torrent has already been downloaded. Get around to watching the stuff' +
+					  ' sometime.');
+			  } else {
+		      $('.container-div').text(`Error ${xhr.status}: ${xhr.statusText}`);
+			  }
+		   
+		  } else { // show the result
+		    $('.container-div').addClass('success');
+		    $('.container-div').text(`Torrent lined up for downloading.`);
 		  }
-	   
-	  } else { // show the result
-	    alert(`Files lined up for downloading.`); // responseText is the server
-	  }
+		};
 	};
 	
-}
+	chrome.storage.sync.get(null, apiCall);
+	
+};
